2f5127bbe361dfdfd738ab807d3285e05e236bef
Christopher Fraser <chris.m.fraser@outlook.com>
2023-05-11 20:41:46 -0400

    Create GPTModule.cs (#1)
    * Create GPTModule.cs
    
    added a command to send a prompt to the gpt-3.5 api without any system message or other context
    
    * Update GPTModule.cs
    
    oops forgot to send the reply :widegarf2:
    
    * Update GPTModule.cs
    
    fixed the api injection and made the message send the actual message

---
+++ Garth/Garth/Modules/GPTModule.cs
@@ -0,0 +1,27 @@
+﻿using ChatGPTCommunicator;
+using ChatGPTCommunicator.Models;
+using ChatGPTCommunicator.Requests.Completion;
+using Discord.Commands;
+using Garth.Helpers;
+
+namespace Garth.Modules;
+
+public class GPTModule : GarthModuleBase
+{
+    
+    private ChatGPT _api;
+
+    public GPTModule(ChatGPT api) { 
+        _api = api;
+    }
+    
+    [Command("gpt")]
+    public async Task GPT([Remainder]string message) {
+        CompletionRequestBuilder chatBuilder = new();
+        
+        chatBuilder.AddMessage(MessageRole.user, message);
+  
+        var response = await _api.SendAsync(chatBuilder.Build());
+        await ReplyAsync(response!.Choices.First().Message.Content);
+    }
+}
